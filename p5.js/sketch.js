fed8d3f8d72867785915cabf847b6bfe24459308
psb-david-petty <psb-david-petty@users.noreply.github.com>
2023-06-19 19:56:20 -0400

    Latest p5.js .JS files (from repl.it).

--- p5.js/sketch.js
+++ p5.js/sketch.js
@@ -1,47 +1,46 @@
 /**
- * BHS Mapping app.
+ * BHS Map app.
  */
-
+//34567890123456789012345678901234567890123456789012345678901234567890
 // Global dimension constants.
 const imageX = 1000, imageY = 1000, divs = 100;
+// Global floor variables
+var floor, f1115, f2115, f3115, f4115, f122t, f222t, f322t, f422t;
+// Global room dictionary.
+let dict = new Object();
 // Global sliders.
 let sliderX, sliderY;
-// Global image variables
-let f1115, f2115, f3115, f4115, f122t, f222t, f322t, f422t;
 // Global text variables.
 let positionText = ``;
 
 function preload() {
-  f2115 = loadImage(`https://psb-2022-2023-apcsp.github.io/map/assets/115-greenough-campus-map-floor-2.png`);
-  f3115 = loadImage(`https://psb-2022-2023-apcsp.github.io/map/assets/115-greenough-campus-map-floor-3.png`);
-}
-
-function mousePressed() {
-  const  // calculate (x,y) lattice point
-    x = Math.round(mouseX / width * divs), 
-    y = Math.round(mouseY / height * divs);
-  positionText = `(${x * width / divs},${y * height / divs})`;
+  [ f1115, f2115, f3115, f4115, f122t, f222t, f322t, f422t, ]
+    = initialize_maps();
 }
 
 function setup() {
   createCanvas(imageX, imageY);
   textSize(32);
   textAlign(CENTER, CENTER);
+  // Create dictionary of rooms.
+  for (const room of rooms) { dict[room.name] = room; }
+  // Configure buttons.
+  setupButtons(10, 20);
+  floor = f3115;
   // Configure sliders.
-  const deltaX = width / divs / 2, deltaY = height / divs / 2;
-  sliderX = createSlider(-deltaX, deltaX, 0, 1);
-  sliderX.position(width / 2 - 100 - 10, height + 10);
-  sliderX.style('width', '100px');
-  sliderY = createSlider(-deltaY, deltaY, 0, 1);
-  sliderY.position(width / 2 + 10, height + 10);
-  sliderY.style('width', '100px');
+  setupSliders(10, 50);
 }
 
 function draw() {
   background(`#eee`);
-  stroke(`#639`)
-  image(f3115, sliderX.value(), sliderY.value());
+  fill(`#639`);
+  stroke(`#639`);
+  strokeWeight(0.25);
   text(`(${sliderX.value()},${sliderY.value()})`, 50, 50);
+  // image(floor.image, sliderX.value(), sliderY.value());
+  image(floor.image, floor.x, floor.y);
+/*
+  // Draw grid.
   for (let x = 0; x <= width; x += width / divs) {
     line(x, 0, x, height);
   }
@@ -49,4 +48,40 @@ function draw() {
     line(0, y, width, y);
   }
   if (positionText) { text(positionText, width / 2, height / 2);}
+*/
+  // TEST: draw the door & entry and connecting arrows.
+  const delta = 25, r = delta / 4, weight = delta / 4;
+  strokeWeight(weight);
+  // Create map of rooms.
+  for (const room of rooms) {
+    if (!thisMap(floor, room.name)) continue;
+    // Draw room dot and door line.
+    let x1 = room.x, y1 = room.y;
+    ellipse(room.x, room.y, r);    // draw dot at (room.x, room.y)
+    if (room.direction) {
+      if (room.direction.toLowerCase() == "s") y1 += delta;
+      if (room.direction.toLowerCase() == "n") y1 -= delta;
+      if (room.direction.toLowerCase() == "e") x1 += delta;
+      if (room.direction.toLowerCase() == "w") x1 -= delta;
+      line(room.x, room.y, x1, y1);  // draw line to door
+      }
+  }
+  for (const room of rooms) {
+    if (!thisMap(floor, room.name)) continue;
+    // Look for room.next array defined.
+    if (room.next !== undefined) {
+      for (const next of room.next) {
+        if (dict[next] !== undefined) {
+          if (thisMap(floor, next)) {
+            // Draw arrow from room to next.
+            arrow(room.x, room.y, dict[next].x, dict[next].y);
+          }
+        }
+      }
+    }
+  }
+  for (const room of rooms) {
+    if (!thisMap(floor, room.name)) continue;
+    annotate(room);
+  }
 }
---
+++ p5.js/util.js
@@ -0,0 +1,120 @@
+/** BHS Map utilities.
+ */
+//34567890123456789012345678901234567890123456789012345678901234567890
+
+/** Return true if floor number matches name number, false otherwise.
+ * floor.name, or name could be empty or undefined.
+ * TODO: must distinguish 115 from UAB & 22t
+ */
+function thisMap(floor, name) {
+  // TODO: correct for UAB && 22 Tappan
+  return floor.name && name && floor.name.endsWith(`115`)
+    && int(floor.name.charAt(1)) == int(name.charAt(1));
+}
+
+/** Initialize maps of each floor:
+ * f1115, f2115, f3115, f4115, f122t, f222t, f322t, f422t
+ */
+function initialize_maps() {
+  return [
+    // f1115
+    { name: `f1115`, legend: `115 Floor 1`, x: -2, y: 2,
+      image: loadImage(`https://psb-2022-2023-apcsp.github.io/map/assets/115-greenough-campus-map-floor-1.png`),
+    },
+    // f2115
+    { name: `f2115`, legend: `115 Floor 2`, x: 3, y: 1,
+      image: loadImage(`https://psb-2022-2023-apcsp.github.io/map/assets/115-greenough-campus-map-floor-2.png`),
+    },
+    // f3115
+    { name: `f3115`, legend: `115 Floor 3`, x: -2, y: -2,
+      image: loadImage(`https://psb-2022-2023-apcsp.github.io/map/assets/115-greenough-campus-map-floor-3.png`),
+    },
+    // f4115
+    { name: ``, legend: ``, x: 0, y: 0, image: undefined, },
+    // f122t
+    { name: `f122t`, legend: `22 Floor 1`, x: 0, y: 0,
+      image: loadImage(`https://psb-2022-2023-apcsp.github.io/map/assets/22-tappan-campus-map-floor-1.png`),
+  },
+    // f222t
+    { name: ``, legend: ``, x: 0, y: 0, image: undefined, },
+    // f322t
+    { name: ``, legend: ``, x: 0, y: 0, image: undefined, },
+    // f422t
+    { name: ``, legend: ``, x: 0, y: 0, image: undefined, },
+  ];
+}
+
+function setupButtons(padR, padT) {
+  const floors = [
+    f1115, f2115, f3115, f4115, f122t, f222t, f322t, f422t,
+  ];
+  const buttonSize = 100,
+    left = (width - (floors.length * (buttonSize + padR))) / 2 + padR,
+    down = height + padT;
+  for (const [i, f, ] of floors.entries()) {
+    f.button = createButton(f.legend);
+    f.button.size(buttonSize);
+    f.button.position(left + i * (buttonSize + padR), down);
+    f.button.mousePressed(() => { floor = f; });
+  }
+}
+
+function setupSliders(padding, extra) {
+  const deltaX = width / divs / 2, deltaY = height / divs / 2;
+  sliderX = createSlider(-deltaX, deltaX, 0, 1);
+  sliderX.position(width / 2 - 100 - padding, height + extra);
+  sliderX.style('width', '100px');
+  sliderY = createSlider(-deltaY, deltaY, 0, 1);
+  sliderY.position(width / 2 + padding, height + extra);
+  sliderY.style('width', '100px');
+}
+
+/**  Draw arrow from source (x1, y1) to dest (x2, y2).
+ */
+function arrow(x1, y1, x2, y2) {
+  // https://stackoverflow.com/a/44892083≥
+  const a2 = 8, a = a2 / 2, weight = 2, color = `#f00`;
+  if (x1 == x2 && y1 == y2) return;
+  let angle = atan2(y1 - y2, x1 - x2);
+  push();
+  strokeWeight(weight); stroke(color); fill(color);
+  line(x1, y1, x2, y2);            // draw line from source to dest
+  translate(x2, y2);               // translate origin to dest
+  rotate(angle-HALF_PI);           // rotate arrow point
+  triangle(-a, a2, +a, a2, 0, -a); // draw arrow point
+  pop();
+}
+
+/** Add room.name annotation next to room position. */
+function annotate(room) {
+  const delta = 75;
+  if (room) {
+    push();
+    textSize(16);
+    textAlign(CENTER, CENTER);
+    strokeWeight(0);
+    fill('blue');
+    let x1 = room.x, y1 = room.y;
+    if (room.direction) {
+      if (room.direction.toLowerCase() == "s") y1 += delta;
+      if (room.direction.toLowerCase() == "n") y1 -= delta;
+      if (room.direction.toLowerCase() == "e") x1 += delta;
+      if (room.direction.toLowerCase() == "w") x1 -= delta;
+    }
+      if (room.name[0] == "t") {
+        fill('green');
+        x1 += delta / 4;
+        y1 -= delta / 4;
+    }
+    text(room.name, x1, y1);
+    pop();
+  }
+}
+
+/** Show (mouseX, mouseY) lattice points. */
+function mousePressed() {
+  const  // calculate (x,y) lattice point
+    x = Math.round(mouseX / width * divs),
+    y = Math.round(mouseY / height * divs);
+  positionText = `(${x * width / divs},${y * height / divs})`;
+}
